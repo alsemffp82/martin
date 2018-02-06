fba1ac2bfef61ea6602418163b58d9d20f360c61
martinjba <chadcho82@hotmail.com>
2018-02-07 07:08:46 +1300

    all posts blade

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,11 +1,48 @@
 @extends('layouts.app')
 
+@section('title', '| All Posts')
+
 @section('content')
     <div class="row">
-        @foreach( $posts as $post )
+        <div class="col-md-10">
+            <h1>All Posts</h1>
+        </div>
+        <div class="col-md-2">
+            <a href="{{ route('posts.create') }}" class="btn btn-lg btn-block btn-primary">Create New Post</a>
+        </div>
+        <div class="col-md-12">
+            <hr>
+        </div>
+
+        {{--  @foreach( $posts as $post )
             <h1>{{ $post->title }}</h1>
             <p>{{ $post->body }}</p>
             <br>
-        @endforeach
+        @endforeach  --}}
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table">
+                <thead>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                    <th>Created At</th>
+                    <th></th>
+                </thead>
+
+                <tbody>
+                    @foreach( $posts as $post)
+                        <tr>
+                            <th>{{ $post->id }}</th>
+                            <td>{{ $post->title }}</td>
+                            <td>{{ substr($post->body, 0, 50) }}{{ strlen($post->body) > 50 ? "..." : "" }}</td>
+                            <td>{{ date( 'M j, Y h:ia', strtotime($post->created_at)) }}</td>
+                            <td><a href="{{ route('posts.show', $post->id) }}" class="btn btn-default">View</a> <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default">Edit</a></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
