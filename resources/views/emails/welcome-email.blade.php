1f42402f889d19ee4a6cbc9e0c2d5b2bdbe85317
martinjba <chadcho82@hotmail.com>
2018-04-10 16:45:47 +1200

    email template

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,239 @@
+<!doctype html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>lahore</title>
+    <style media="all" type="text/css">
+    @media  only screen and (max-width: 620px) {
+      .span-2,
+      .span-3 {
+        max-width: none !important;
+        width: 100% !important;
+      }
+      .span-2 > table,
+      .span-3 > table {
+        max-width: 100% !important;
+        width: 100% !important;
+      }
+    }
+
+    @media  all {
+      .btn-primary table td:hover {
+        background-color: #34495e !important;
+      }
+      .btn-primary a:hover {
+        background-color: #34495e !important;
+        border-color: #34495e !important;
+      }
+    }
+
+    @media  all {
+      .btn-secondary a:hover {
+        border-color: #34495e !important;
+        color: #34495e !important;
+      }
+    }
+
+    @media  only screen and (max-width: 620px) {
+      h1 {
+        font-size: 28px !important;
+        margin-bottom: 10px !important;
+      }
+      h2 {
+        font-size: 22px !important;
+        margin-bottom: 10px !important;
+      }
+      h3 {
+        font-size: 16px !important;
+        margin-bottom: 10px !important;
+      }
+      p,
+      ul,
+      ol,
+      td,
+      span,
+      a {
+        font-size: 16px !important;
+      }
+      .wrapper,
+      .article {
+        padding: 10px !important;
+      }
+      .content {
+        padding: 0 !important;
+      }
+      .container {
+        padding: 0 !important;
+        width: 100% !important;
+      }
+      .header {
+        margin-bottom: 10px !important;
+      }
+      .main {
+        border-left-width: 0 !important;
+        border-radius: 0 !important;
+        border-right-width: 0 !important;
+      }
+      .btn table {
+        width: 100% !important;
+      }
+      .btn a {
+        width: 100% !important;
+      }
+      .img-responsive {
+        height: auto !important;
+        max-width: 100% !important;
+        width: auto !important;
+      }
+      .alert td {
+        border-radius: 0 !important;
+        padding: 10px !important;
+      }
+      .receipt {
+        width: 100% !important;
+      }
+    }
+
+    @media  all {
+      .ExternalClass {
+        width: 100%;
+      }
+      .ExternalClass,
+      .ExternalClass p,
+      .ExternalClass span,
+      .ExternalClass font,
+      .ExternalClass td,
+      .ExternalClass div {
+        line-height: 100%;
+      }
+      .apple-link a {
+        color: inherit !important;
+        font-family: inherit !important;
+        font-size: inherit !important;
+        font-weight: inherit !important;
+        line-height: inherit !important;
+        text-decoration: none !important;
+      }
+    }
+    </style>
+
+  </head>
+
+  <!-- Before using this email template check all color and images -->
+  <!-- Before using this email template check all color and images -->
+  <!-- Before using this email template check all color and images -->
+  <!-- Before using this email template check all color and images -->
+  
+  <body class="" style="font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; background-color: #f6f6f6; margin: 0; padding: 0;">
+    <table border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: #f6f6f6;" width="100%" bgcolor="#f6f6f6">
+      <tr>
+        <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;" valign="top">&nbsp;</td>
+        <td class="container" style="font-family: sans-serif; font-size: 14px; vertical-align: top; Margin: 0 auto !important; max-width: 580px; padding: 10px; width: 580px;" width="580" valign="top">
+          <div class="content" style="box-sizing: border-box; display: block; Margin: 0 auto; max-width: 580px; padding: 10px;">
+
+            <!-- START CENTERED WHITE CONTAINER -->
+            <!-- <span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">This is preheader text. Some clients will show this text as a preview.</span> -->
+
+            <!-- START HEADER -->
+            <div class="header" style="margin-bottom: 20px; Margin-top: 10px; width: 100%;">
+              <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; min-width: 100%;" width="100%">
+                <tr>
+                  <td class="align-center" style="font-family: sans-serif; font-size: 14px; vertical-align: top; text-align: center;" valign="top" align="center">
+                    <!-- <a href="http://lahore.localhost" target="_blank" style="color: #3498db; text-decoration: underline;">
+                        <img src="http://lahore.localhost/images/theme-lahore/logo-email-header.png" width="156" alt="Logo" align="center" style="border: none; -ms-interpolation-mode: bicubic; max-width: 100%;">
+                    </a> -->
+                  </td>
+                </tr>
+              </table>
+            </div>
+
+            <!-- END HEADER -->
+            <table border="0" cellpadding="0" cellspacing="0" class="main" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background: #fff; border-radius: 3px;" width="100%">
+
+              <tr>
+                <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;" valign="top">
+                    
+        <table border="0" cellpadding="0" cellspacing="0" class="alert alert-success" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; min-width: 100%;" width="100%">
+            <tr>
+                <td align="center" style="font-family: sans-serif; vertical-align: top; font-size: 16px; border-radius: 3px 3px 0 0; color: #ffffff; font-weight: 500; padding: 20px; text-align: center; background-color: #87A103" valign="top" bgcolor="#87A103"> Welcome to University of Lahore</td>
+            </tr>
+        </table>
+    
+                </td>
+            </tr>
+
+            <!-- START MAIN CONTENT AREA -->
+            <tr>
+                    
+        <td class="wrapper" style="font-family: sans-serif; font-size: 14px; vertical-align: top; box-sizing: border-box; padding: 20px;" valign="top">
+            <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" width="100%">
+                <tr>
+                    <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;" valign="top">
+                        <!-- <p style="font-family: sans-serif; font-size: 16px; margin: 0; Margin-bottom: 15px; color: #87A103!important">Welcome new employer test,</p>
+                        <p style="font-family: sans-serif; font-size: 16px; margin: 0; Margin-bottom: 15px;">My name is Hamid and I look after our business customers at University of Lahore.</p>
+                        <p style="font-family: sans-serif; font-size: 16px; margin: 0; Margin-bottom: 15px;">Your time is precious, and with that in mind please allow me to briefly highlight some key aspects of our innovative platform to help you to recruit qualified candidates faster:</p>
+                        <p style="font-family: sans-serif; font-size: 16px; margin: 0; Margin-bottom: 15px;">Self Discovery Tool - our candidates become aware of their strengths, weaknesses and potential skills. We allow them to boost these skills through our online courses to match your requirements.</p>
+                        <p style="font-family: sans-serif; font-size: 16px; margin: 0; Margin-bottom: 15px;">Course Output Document - gain a deeper insight into the personal views of each candidate, such as industry skills and situations, to easily find candidates who are your company vision.</p>
+                        <p style="font-family: sans-serif; font-size: 16px; margin: 0; Margin-bottom: 15px;">Whether you've joined us to post a job or simply want to check out our talent pool, we’re excited to have you on board. When you’re ready to list a job, simply <a href="http://lahore.localhost/opportunities">click here</a> to list your opportunity to the community.</p>
+                        <p style="font-family: sans-serif; font-size: 16px; margin: 0; Margin-bottom: 15px;">We also offer a JBA Enterprise Solution for Businesses. To find out more, please <a href="https://pk.joybusinessacademy.com/business">click here</a></p>
+                        <p style="font-family: sans-serif; font-size: 16px; margin: 0; Margin-bottom: 15px;">If you have any questions feel free to contact me any time.</p>
+                        <p style="font-family: sans-serif; font-size: 16px; margin: 0; Margin-bottom: 15px;">Thank you,</p>
+                        <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 0px;">Hamid Jilani</p>
+                        <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 0px;">Manager Strategic Alliances</p>
+                        <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 0px;">m: +92 333 2012 553</p> -->
+
+                        <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box; min-width: 100% !important;" width="100%">
+                            <tbody>
+                                <tr style="padding-top: 15px;">
+                                    <td align="center" style="font-family: sans-serif; font-size: 14px; vertical-align: top; padding-bottom: 15px; padding-top: 15px;" valign="top">
+                                        <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">
+                                            <tbody>
+                                                <tr>
+                                                    <!-- <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #87A103; border-radius: 5px; text-align: center;" valign="top" bgcolor="#87A103" align="center"> <a href="http://lahore.localhost" target="_blank" style="display: inline-block; color: #ffffff; background-color: #87A103; border: solid 1px #87A103; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; border-color: #87A103">Get started!</a></td> -->
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div style="width:100%; text-align: left; padding-left: 20px;">
+                            <!-- <a target="_blank" title="find us on Facebook" href="https://www.facebook.com/University.Lahore"><img style="width:30px; height:30px;" src="http://lahore.localhost/images/logos/lahore-facebook.png" border=0></a>
+                            <a target="_blank" title="find us on Twitter" href="https://twitter.com/ULahore"><img style="width:30px; height:30px;" src="http://lahore.localhost/images/logos/lahore-twitter.png" border=0></a>
+                            <a target="_blank" title="find us on Google Plus" href="https://plus.google.com/u/0/+universityoflahore"><img style="width:30px; height:30px;" src="http://lahore.localhost/images/logos/lahore-google-plus.png" border=0></a> -->
+                        </div>
+                    </td>
+                </tr>
+            </table>
+
+        </td>
+        
+              </tr>
+
+              <!-- END MAIN CONTENT AREA -->
+              </table>
+
+            <!-- START FOOTER -->
+                <div class="footer" style="clear: both; padding-top: 10px; text-align: center; width: 100%;">
+                    <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" width="100%">
+                                                <tr>
+                            <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-top: 10px; padding-bottom: 10px; font-size: 12px; color: #999999; text-align: center;" valign="top" align="center">
+                                <!-- <p class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">1 - KM Defence Road, Lahore (Lahore Campus)</p>
+                                <p class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">6.7 KM Japan Road, Sihala Zone V, Islamabad (Islamabad Campus)</p>
+                                <p class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">Adjacent Chenab Bridge , GT Road Gujrat (Gujrat Campus)</p>
+                                <p class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">Near Chanwat - Sahiwal Road, Pakpattan (Pakpattan Campus)</p> -->
+                        </tr>
+                    </table>
+              </div>
+            <!-- END FOOTER -->
+
+            <!-- END CENTERED WHITE CONTAINER -->
+          </div>
+        </td>
+        <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;" valign="top">&nbsp;</td>
+      </tr>
+    </table>
+        <!-- <img src="http://lahore.localhost/pixel.gif?id=MTg1Nzh8d2VsY29tZQ==" style="display:none;height:1px;opacity:0;width:1px;" height="1px" width="1px"> -->
+  </body>
+</html>
